683ab2c175b41459741218a6d1b62f0f3680bbd7
unknown <dmitriy1991dp@gmail.com>
2020-06-01 18:04:46 +0300

    tanatos_studio

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+//Swiper Slider for Review Section
+var swiper = new Swiper('.slider-1', {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed: 600,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var swiper = new Swiper('.slider-2', {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+
+    }
+});
+
+//wow js
+new WOW().init();
